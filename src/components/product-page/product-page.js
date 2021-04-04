aef13186c7a36d3b15f6ad8817bc6574db8ef238
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-05 00:46:17 +0530

    update : added some objects to render view according to certain condition

--- src/components/product-page/product-page.js
+++ src/components/product-page/product-page.js
@@ -1,23 +1,47 @@
-import {data} from '../faker'
+import { useState } from 'react'
 import {useEcom} from '../ecom-context/ecom-context'
 import '../product-page/product-page.css'
 
 export function Products(){
-    const {dispatch} = useEcom()
+    const {state , dispatch} = useEcom()
+    const {data} = state 
+    console.log("data is " , data)
+
+
+    
+    const clicked = {
+        currentClass : "button button-secondary" , 
+        currentText : "✅Added To Wishlist" , 
+        visibility : true
+    }
+    const  unclicked = {
+        currentClass : "button button-success" , 
+        currentText : "Add To Wishlist" , 
+        visibility : false
+    }         
+
     return(
         <div className = "products-main">
             {data.map(product => 
-
+           
                 <div class="card-badge">
                     <img class="image-resize" src={product.image} /> 
                     <div class="card-badge-content"> 
                         <p class="bold">{product.productName}</p>
                         <p>{product.description}</p>
                         <span class="bold">Rs {product.price} </span>
-                       <button class="button button-success" id = {product.id} onClick = {() => dispatch({type : 'ADD_TO_WISHLIST' , payload : product})}>Add To Wishlist</button>
+                       <button 
+                        class = {product.wishlistStatus ?   clicked.currentClass : unclicked.currentClass} 
+                        disabled =  {product.wishlistStatus ?   clicked.visibility : unclicked.visibility} 
+                        id = {product.id} 
+                        onClick = {() =>
+                        { dispatch({type : 'ADD_TO_WISHLIST' , payload : product})
+                        }}>
+                            {product.wishlistStatus ?  clicked.currentText : unclicked.currentText}
+                        </button>
                     </div>
                 </div>
-            )}
+)}
 
 
         </div>
