04d6c1d6845a3d2211275fb0384d22e9f6ddef21
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-05 00:47:12 +0530

    update : added a button

--- src/components/Wishlist/wishlist.js
+++ src/components/Wishlist/wishlist.js
@@ -3,7 +3,8 @@ import '../product-page/product-page.css'
 import './wishlist-main.css'
 export function Wishlist(){
     const {state  , dispatch} = useEcom()
-    const {wishlist} = state
+    const {wishlist , wishlistStatus} = state
+    
     if(wishlist.length === 0){
         return(
             <h1>Wishlist is Empty</h1>
@@ -22,7 +23,8 @@ export function Wishlist(){
                         <p>{product.description}</p>
                         <span class="bold">Rs {product.price} </span>
                         <div className = "buttons-wishlist">
-                       <button class="button button-success" id = {product.id} onClick = {() => dispatch({type : 'ADD_TO_CART' , payload : product})}>Add To Cart</button>
+                            {/* {{wishlistStatus} ? className = "button button-success" : className = "button button-secondary"} */}
+                       <button  class="button button-primary" id = {product.id} onClick = {() => dispatch({type : 'ADD_TO_CART' , payload : product})}>Add To Cart</button>
                         <button class="button button-warning" id = {product.id} onClick = {() => dispatch({type : 'REMOVE_FROM_WISHLIST' , payload : product})}>Remove From Wishlist</button>
                         </div>
                     </div>
