717f2c1c2900cc8e7a97db9520cc8cbfa93c26ac
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-06 21:09:39 +0530

    update : added some functionalities

--- src/components/ecom-context/ecom-context.js
+++ src/components/ecom-context/ecom-context.js
@@ -24,21 +24,34 @@ function  ecomReducer(state  , {type , payload}){
             return {...state  , route : {...route , value : 'wishlist'}}
             
         // second part to handle cart and wishlist
+
+        // state is an array with objects data and wishlist
+        // payload is coming product
+        // when payload is  coming simply add that to wishlist
+        // but update the data on the main products page so that same item can't be added to wishlist again
+        // here for that i am putting a property of wishlistStatus : true(for items being added to wishlist) on the main data which is being mapped on products page
+        // it works but only once , state is not persisting
+        // when i click ADD TO WISHSLIST that product goes disabled which i want but when i click any other item that one goes disabled but previous item looses the property of wishlistStatus and gets enabled
         case 'ADD_TO_WISHLIST':
-            return {...state  ,  wishlist : [...wishlist , payload] , 
-                data :  data.map(product => product.id === payload.id ? {...product , wishlistStatus : true } :  {...product} ) }
-
+            return {...state  , wishlist :  [...wishlist , payload] ,  
+                data :  data.map(product => product.id === payload.id ? {...product , wishlistStatus : true } :  {...product} ) } 
         case 'ADD_TO_CART':
-
             return {...state  , wishlist : wishlist.filter((item) => item.id !== payload.id) ,  cart : [...cart ,  {...payload , quantity : 1}]}
-
         case 'REMOVE_FROM_CART':
             return {...state  , cart : cart.filter(product => product.id !== payload.id)}
         case 'MOVE_TO_WISHLIST':
             return {...state  , wishlist : [...wishlist , payload] , cart : cart.filter(product => product.id !== payload.id)}
         case 'REMOVE_FROM_WISHLIST' :
             return {...state , wishlist : wishlist.filter(product => product.id !== payload.id)}
-            default:
+        case 'INCREASE_QUANTITY':
+            return {...state , cart : cart.map(product => product.id === payload.id ? {...product , quantity : product.quantity+1} : {...product})}
+         case 'DECREASE_QUANTITY':
+             if(payload.quantity  <2){
+                 return {...state , cart : cart.filter((product) => product.id !== payload.id )}
+             }
+            return {...state , cart : cart.map(product => product.id === payload.id ? {...product , quantity : product.quantity-1} : {...product})}
+
+        default:
            return  {...state}
 
     }
