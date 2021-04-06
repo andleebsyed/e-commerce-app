bfc11bcb5838c667b7c086815e140e51ee0e2e9d
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-06 21:12:27 +0530

    update : creation of checkout component

---
+++ src/components/Checkout/checkout.js
@@ -0,0 +1,16 @@
+import { useEcom } from "../ecom-context/ecom-context"
+import './checkout.css'
+export function Checkout(){
+   const {state} = useEcom()
+   const {cart} = state
+   const totalItems = cart.reduce((totalQunatity , product) => totalQunatity+product.quantity , 0 )
+   const totalPrice = cart.reduce((totalPrice , product) => totalPrice + product.price*product.quantity , 0)  
+    return(
+        <div className = "checkout-main">
+            <h1>Checkout</h1>
+            <p>Total Items : {totalItems} </p>
+            <p>Total Price : {totalPrice}</p>
+            <button className= "button button-primary">Proceed To Pay</button>
+        </div>
+    )
+}
