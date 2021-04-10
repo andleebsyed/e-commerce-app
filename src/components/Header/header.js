c6079d186db13db71339fd0bc84bd51902beb457
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-10 11:48:14 +0530

    update : made search functionality work

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,28 @@
 import './header.css'
 import {useEcom} from '../ecom-context/ecom-context'
+import { useState } from 'react';
 export function Header(){
     const {state , dispatch} = useEcom();
-    const { wishlist , cart } = state;
+    const { wishlist , cart , data } = state;
+    // state for searching of an item
+    const [comingProd , setComingProd] = useState('');
+
+    // function searchFieldHandler(event){
+    //     const enteredItem = event.target.value;
+    //     setComingProd(event.target.value)
+
+
+    // }
+    // function searchButtonHandler(){
+
+    //     console.log(  "item to search " ,  comingProd) 
+
+    // }
     return(
         <div>
             <div>
-                <input type = "text" id = "search-box" placeholder = "Type here to Search"/>
+                <input type = "text" id = "search-box" placeholder = "Type here to Search" onChange = {(event) => setComingProd(event.target.value)}/>
+                <button onClick ={() => dispatch({type : 'SEARCH_FOR_ITEM' , payload : comingProd})} >Search</button>
             </div>
             <nav className = "navbar-main">
                 <div>
