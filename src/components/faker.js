f7dd7655e5fe1f0e9ce401865dbd941ad8ae2200
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-10 11:47:36 +0530

    update : added inStock and fastDelivery properties to data coming from faker

--- src/components/faker.js
+++ src/components/faker.js
@@ -1,12 +1,14 @@
 import faker, { fake } from 'faker'
 faker.seed(123)
-export const data =  [...Array(10)].map(item =>
+export const data =  [...Array(13)].map(item =>
     ({
     id: faker.datatype.uuid() ,
     productName  : faker.commerce.productName(),
     description  : faker.commerce.productDescription(),
     price : faker.commerce.price() , 
-    image : faker.image.fashion()
+    image : faker.image.fashion() , 
+    inStock: faker.datatype.boolean(),
+    fastDelivery: faker.datatype.boolean(),
     })
 )
 
