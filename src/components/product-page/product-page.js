3302c6ca59d3d923eb4f70ebe077db8d5a3a0910
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-13 01:33:08 +0530

    update : used Cart component to make products page

--- src/components/product-page/product-page.js
+++ src/components/product-page/product-page.js
@@ -1,56 +1,17 @@
 import { useState } from 'react'
-import {useEcom} from '../ecom-context/ecom-context'
-import {Card} from '../ProductCard/card'
+import { useEcom } from '../ecom-context/ecom-context'
+import { Card } from '../ProductCard/card'
 import '../product-page/product-page.css'
 
-export function Products(){
-    const {state , dispatch} = useEcom()
-    const {data , wishlist} = state 
-    console.log("data is " , data)
-
-
-    
-    const clicked = {
-        currentClass : "button button-secondary" , 
-        currentText : "✅Added To Wishlist" , 
-        visibility : true
-    }
-    const  unclicked = {
-        currentClass : "button button-success" , 
-        currentText : "Add To Wishlist" , 
-        visibility : false
-    }         
-    let currentStyleStatus;
-
-    return(
-        <div className = "products-main">
-            {data.map(product => 
-
-                <Card product = {product} />
-                // <div class="card-badge">
-                //     <img class="image-resize" src={product.image} /> 
-                //     <div class="card-badge-content"> 
-                //         <p class="bold">{product.productName}</p>
-                //         <p>{product.description}</p>
-                //         <span class="bold">Rs {product.price} </span>
-                //        <button  
-                      
-                //         {...currentStyleStatus =  wishlist.find(item => item.id === product.id ? true : false) } 
-                //           disabled ={currentStyleStatus}
-                //         className = {currentStyleStatus ? clicked.currentClass : unclicked.currentClass} 
-                       
-                       
-                //         id = {product.id} 
-                //         onClick = {() =>
-                //         { dispatch({type : 'ADD_TO_WISHLIST' , payload : product})
-                //         }}>
-                //             {currentStyleStatus ? clicked.currentText : unclicked.currentText}
-                //         </button>
-                //     </div>
-                // </div>
-)}
-
-
+export function Products() {
+    const { state } = useEcom()
+    const { data } = state
+    return (
+        <div className="products-main">
+            {data.map(product =>
+                <Card product={product} />
+            )}
         </div>
+
     )
 }
