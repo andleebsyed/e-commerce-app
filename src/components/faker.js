03c1c83fb7a552c5e6849c95f49ef4cd18e24f83
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-15 14:59:37 +0530

    refactor : refactored code a bit

--- src/components/faker.js
+++ src/components/faker.js
@@ -1,16 +1,13 @@
 import faker, { fake } from 'faker'
 faker.seed(123)
-export const data =  [...Array(13)].map(item =>
+export const data = [...Array(15)].map(item =>
     ({
-    id: faker.datatype.uuid() ,
-    productName  : faker.commerce.productName(),
-    // description  : faker.commerce.productDescription(),
-    description : faker.lorem.words() , 
-    price : faker.commerce.price() , 
-    image : faker.image.fashion() , 
-    inStock: faker.datatype.boolean(),
-    fastDelivery: faker.datatype.boolean(),
+        id: faker.datatype.uuid(),
+        productName: faker.commerce.productName(),
+        description: faker.lorem.words(),
+        price: faker.commerce.price(),
+        image: faker.image.fashion(),
+        inStock: faker.datatype.boolean(),
+        fastDelivery: faker.datatype.boolean(),
     })
 )
-
-// console.log(data)
