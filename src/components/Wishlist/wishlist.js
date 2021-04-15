547c0805ef522721b19c549539dc1d87161cc12a
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-15 15:05:25 +0530

    Update wishlist.js

--- src/components/Wishlist/wishlist.js
+++ src/components/Wishlist/wishlist.js
@@ -1,37 +1,43 @@
-import {useEcom} from '../ecom-context/ecom-context'
+import { useEcom } from '../ecom-context/ecom-context'
 import '../product-page/product-page.css'
 import './wishlist-main.css'
-export function Wishlist(){
-    const {state  , dispatch} = useEcom()
-    const {wishlist} = state
-    
-    if(wishlist.length === 0){
-        return(
+import '../ProductCard/card'
+import '../ProductCard/card.css'
+export function Wishlist() {
+    const { state, dispatch } = useEcom()
+    const { wishlist } = state
+
+    if (wishlist.length === 0) {
+        return (
             <h1>Wishlist is Empty</h1>
         )
     }
-    else{
-        return(
-            <div className = "wishlist-main">
-                {wishlist.map(product => 
-                    <div class = "ecom-card">
-                        <img class = "card-image" src = {product.image} />
+    else {
+        return (
+            <div className="container-div">
+                <h1>Wishlist</h1>
+                <div className="wishlist-main">
+                    {/* <>Wishlist</h1> */}
+                    {wishlist.map(product =>
+                        <div class="ecom-card">
+                            <img class="card-image" src={product.image} />
 
-                
-                        <div class = "card-info">
-                            <strong>{product.productName}</strong> 
-                            <p>{product.description}</p>
-                            <strong>Rs {product.price}</strong>
-                        </div>
-                            
-                            <div className = "buttons-wishlist">
-                                <button  class="button button-primary" id = {product.id} onClick = {() => dispatch({type : 'ADD_TO_CART' , payload : product})}>Add To Cart</button>
-                                <button class="button button-warning" id = {product.id} onClick = {() => dispatch({type : 'REMOVE_FROM_WISHLIST' , payload : product})}>Remove From Wishlist</button>
+
+                            <div class="card-info">
+                                <strong>{product.productName}</strong>
+                                <p>{product.description}</p>
+                                <strong>Rs {product.price}</strong>
                             </div>
-                    </div>
-                )}
+
+                            <div className="buttons-wishlist">
+                                <button class="button button-success" id={product.id} onClick={() => dispatch({ type: 'ADD_TO_CART', payload: product })}>Add To Cart</button>
+                                <button class="button button-warning" id={product.id} onClick={() => dispatch({ type: 'REMOVE_FROM_WISHLIST', payload: product })}>Remove From Wishlist</button>
+                            </div>
+                        </div>
+                    )}
 
 
+                </div>
             </div>
         )
     }
