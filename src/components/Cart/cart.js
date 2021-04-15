e35b73cc3babfbe2531c41d53c9d2edf845bf9ab
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-15 14:58:25 +0530

    style : styled up cart component a bit

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -1,61 +1,49 @@
-import {Checkout} from '../Checkout/checkout'
-import {useEcom} from '../ecom-context/ecom-context'
+import { Checkout } from '../Checkout/checkout'
+import { useEcom } from '../ecom-context/ecom-context'
 import './cart.css'
-export function Cart(){
-    const {state , dispatch} = useEcom()
-    const {cart} = state
-    if(cart.length ===0){
-        return(
+export function Cart() {
+    const { state, dispatch } = useEcom()
+    const { cart } = state
+    if (cart.length === 0) {
+        return (
             <h1>Cart is Empty</h1>
         )
     }
-    else{
-        console.log("cart is " ,  cart)
-        return(
+    else {
+        console.log("cart is ", cart)
+        return (
             <div>
-               
-            <h1>Cart</h1>
-            {cart.map(
-                product => 
-                    <div class = "ecom-card">
-                        <img class = "card-image" src = {product.image} />
 
-                
-                        <div class = "card-info">
-                            <strong>{product.productName}</strong> 
-                            <p>{product.description}</p>
-                            <strong>Rs {product.price}</strong>
-                        </div>
-                            <div class = "quantity-manager">
-                                 <button onClick = {() => dispatch({type : 'DECREASE_QUANTITY' , payload : product})}>-</button>
-                                <span class = "quantity-count">{product.quantity}</span>
-                                <button onClick = {() => dispatch({type : 'INCREASE_QUANTITY' , payload : product})}>+</button>
-                              </div>
-                            <div className = "cart-buttons">
-                                <button  class="button button-primary" id = {product.id} onClick = {() => dispatch({type : 'MOVE_TO_WISHLIST' , payload : product})}>Move To Wishlist</button>
-                                <button class="button button-warning" id = {product.id} onClick = {() => dispatch({type : 'REMOVE_FROM_CART' , payload : product})}>Remove From Cart</button>
+                <h1>Cart</h1>
+                <div className="cart-main">
+                    {cart.map(
+                        product =>
+
+                            <div class="ecom-card">
+                                <img class="card-image" src={product.image} />
+
+
+                                <div class="card-info">
+                                    <strong>{product.productName}</strong>
+                                    <p>{product.description}</p>
+                                    <strong>Rs {product.price}</strong>
+                                </div>
+                                <div class="quantity-manager">
+                                    <button onClick={() => dispatch({ type: 'DECREASE_QUANTITY', payload: product })}>-</button>
+                                    <span class="quantity-count">{product.quantity}</span>
+                                    <button onClick={() => dispatch({ type: 'INCREASE_QUANTITY', payload: product })}>+</button>
+                                </div>
+                                <div className="cart-buttons">
+                                    <button class="button button-success" id={product.id} onClick={() => dispatch({ type: 'MOVE_TO_WISHLIST', payload: product })}>Move To Wishlist</button>
+                                    <button class="button button-warning" id={product.id} onClick={() => dispatch({ type: 'REMOVE_FROM_CART', payload: product })}>Remove From Cart</button>
+                                </div>
                             </div>
-                    </div>
-                    // <div class="card-badge">
-                    //     <img class="image-resize" src={product.image} /> 
-                    //     <div class="card-badge-content"> 
-                    //         <p class="bold">{product.productName}</p>
-                    //         <p>{product.description}</p>
-                    //         <span class="bold">Rs {product.price} </span><br />
-                    //         <div class = "quantity-manager">
-                    //             <button onClick = {() => dispatch({type : 'DECREASE_QUANTITY' , payload : product})}>-</button>
-                    //                 <span class = "quantity-count">{product.quantity}</span>
-                    //             <button onClick = {() => dispatch({type : 'INCREASE_QUANTITY' , payload : product})}>+</button>
-                    //          </div>
-                    //         <div class = "cart-buttons">
-                               
-                    //         <button class="button button-success" id = {product.id} onClick = {() => dispatch({type : 'MOVE_TO_WISHLIST' , payload : product})}>Move To Wishlist</button>
-                    //         <button class="button button-warning" id = {product.id} onClick = {() => dispatch({type : 'REMOVE_FROM_CART' , payload : product})}>Remove From Cart</button>
-                    //         </div>
-                    //     </div>
-                    // </div>
-                )}
-                 <Checkout />
+
+
+                    )}
                 </div>
-        )}
+                <Checkout />
+            </div>
+        )
+    }
 }
