4ce676c0a8837a48dd5a010b03e39349d2759829
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-18 14:43:17 +0530

    style : did some changes in style

--- src/components/EmptyWishlist/EmptyWishlist.js
+++ src/components/EmptyWishlist/EmptyWishlist.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export function EmptyWishlist() {
     return (
         <div className="empty-wishlist-main">
-            <i class="material-icons large-cart">favorite_border</i>
+            <i class="material-icons large-wishlist">favorite_border</i>
             <strong>Uh oh!! There's Nothing Here </strong>
             <Link to='/products' className="button button-primary link-style">Explore Maybe?</Link>
         </div>
