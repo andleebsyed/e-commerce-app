c8dc2c9a534d2118827c43aaa91b6d771382cbe3
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-28 00:37:35 +0530

    update : maintained a state which is used for view on the basis of operations on database

--- src/components/ecom-context/ecom-context.js
+++ src/components/ecom-context/ecom-context.js
@@ -9,50 +9,47 @@ const EcomContext = createContext()
 
 
 // reducer function
-async function ecomReducer(state, { type, payload }) {
+function ecomReducer(state, { type, payload }) {
     const { wishlist, cart } = state
     console.log("cart after ist render ", cart)
     // const orgData = data;
+    console.log("state initially in switch ", state)
     switch (type) {
+
         case 'INITIAL_CART':
-            console.log("cart and wishlist from state ", cart, wishlist)
-            console.log("initial cart as a payload from db is ", payload)
+            return { ...state, cart: [...payload] }
+        case 'INITIAL_WISHLIST':
+            return { ...state, wishlist: [...payload] }
 
-            const temp = { ...state, cart: [...cart, ...payload] }
-            console.log("temp is ", temp.cart)
-            return temp
-        // case 'ADD_TO_WISHLIST':
-        //     try {
-        //         const response = await axios.post('https://rest-api.andydev7.repl.co/wishlist', payload)
-        //         if (response.status === 200) { return { ...state, wishlist: [...wishlist, payload] } }
-        //     }
-        //     catch (error) {
-        //         console.log(error.message)
-        //         // console.log(error.response.data);
-        //         // console.log(error.response.status);
-        //         // console.log(error.response.headers);
-        //     }
+        case 'ADD_TO_WISHLIST':
+            return { ...state, wishlist: [...wishlist, payload] }
         case 'ADD_TO_CART':
-            try {
-                console.log("cart before adding new item ", cart)
-                const response = await axios.post('https://rest-api.andydev7.repl.co/cart', payload)
 
-                if (response.status === 200) {
-                    console.log("new item being added is ", payload)
-                    return { ...state, cart: [...cart, ...payload] }
+            return { ...state, cart: [...cart, payload] }
+        // console.log("new item being added is ", payload)
+        // const temp = { ...state, cart: [...cart, payload] }
+        //  console.log("state after update is   ", temp)
+        // try {
+        //     console.log("cart before adding new item ", cart)
+        //     const response = await axios.post('https://rest-api.andydev7.repl.co/cart', payload)
 
-                    // return { ...state, wishlist: wishlist.filter((item) => item._id !== payload._id), cart: [...cart, { ...payload, quantity: 1 }] }
-                }
+        //     if (response.status === 200) {
+        //         console.log("new item being added is ", payload)
+        //         const temp = { ...state, cart: [...cart, payload] }
+        //         console.log("state after update is   ", temp)
 
-            }
-            catch (error) {
-                console.log("err message is ", error.message)
-            }
+        //         // return { ...state, wishlist: wishlist.filter((item) => item._id !== payload._id), cart: [...cart, { ...payload, quantity: 1 }] }
+        //     }
+
+        // }
+        // catch (error) {
+        //     console.log("err message is ", error.message)
+        // }
         // return { ...state, wishlist: wishlist.filter((item) => item.id !== payload.id), cart: [...cart, { ...payload, quantity: 1 }] }
-        // case 'REMOVE_FROM_CART':
-        //     return { ...state, cart: cart.filter(product => product.id !== payload.id) }
-        // case 'MOVE_TO_WISHLIST':
-        //     return { ...state, wishlist: [...wishlist, payload], cart: cart.filter(product => product.id !== payload.id) }
+        case 'REMOVE_FROM_CART':
+            return { ...state, cart: cart.filter(product => product._id !== payload._id) }
+        case 'MOVE_TO_WISHLIST':
+            return { ...state, wishlist: [...wishlist, payload], cart: cart.filter(product => product._id !== payload._id) }
         // case 'REMOVE_FROM_WISHLIST':
         //     return { ...state, wishlist: wishlist.filter(product => product.id !== payload.id) }
         // case 'INCREASE_QUANTITY':
@@ -77,16 +74,40 @@ async function ecomReducer(state, { type, payload }) {
         // case 'REMOVE_ALL_CONDITIONS':
         //     return { ...state, data: orgData }
         default:
-            return { ...state }
+            return state
 
     }
 }
 
 export function EcomProvider({ children }) {
-    // let wishlist = []
-    // let cart = []
-    // const initialState = { wishlist, cart }
+
     const [state, dispatch] = useReducer(ecomReducer, { cart: [], wishlist: [] })
+    console.log("state status ", state)
+
+
+    useEffect(() => {
+
+        async function MyCart() {
+            const response = await axios.get('https://rest-api.andydev7.repl.co/cart')
+            dispatch({ type: 'INITIAL_CART', payload: response.data.myCart })
+        }
+        MyCart()
+    }, [])
+
+    useEffect(() => {
+        async function MyWishlist() {
+            console.log("when is wishlist being fired")
+            const response = await axios.get('https://rest-api.andydev7.repl.co/wishlist')
+            console.log("wishlist coming from db ", response.data.myWishlist, " and its type is ", typeof (response.data.myWishlist))
+            dispatch({ type: 'INITIAL_WISHLIST', payload: response.data.myWishlist })
+        }
+
+        MyWishlist()
+
+    }, [])
+
+
+
     return (
         <EcomContext.Provider value={{ state, dispatch }}>
             {children}
