4a5aaff125d276212eb18d8a9e8c051777ed35dd
Syed Andleeb <andleebsyed75@gmail.com>
2021-04-29 03:01:06 +0530

    update : added Loader to products page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Header } from './components/Header/header'
 import { Wishlist } from './components/Wishlist/wishlist'
 import { Cart } from './components/Cart/cart'
 import { Route, Routes } from 'react-router-dom'
-// import { useState, useEffect } from 'react'
-
 import axios from 'axios'
+import { useEcom } from './components/ecom-context/ecom-context';
+import { Loader } from './components/Loader/Loader';
 
 function App() {
+  const { loader } = useEcom()
   return (
     <div className="main-div">
+      <Loader loader={loader} />
       <div className="app-header"><Header /></div>
+
       <Routes>
+
         <Route path="/wishlist" element={<div className="wishlist"><Wishlist /></div>} />
 
         <Route path="/products" element={<div className="products"><Products /></div>} />
