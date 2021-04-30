6dc3974b306570d7698f656bed44b8d57f3150d4
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-01 03:17:00 +0530

    update : loader on action of adding item to wishlist

--- src/components/ProductCard/card.js
+++ src/components/ProductCard/card.js
@@ -2,9 +2,9 @@ import './card.css'
 import { useEcom } from '../ecom-context/ecom-context'
 import { useDatabase } from '../DatabaseCalls/DatabaseCalls'
 import { AddToCart } from '../utils/Operations'
-import { AddToWishlist } from '../utils/Operations'
+import { AddToWishlist, RemoveFromWishlist } from '../utils/Operations'
 export function Card({ product }) {
-    const { state, dispatch } = useEcom()
+    const { state, dispatch, loader, setLoader } = useEcom()
     // const { wishlist } = useDatabase()
     const { cart, wishlist } = state
     const clicked = {
@@ -32,13 +32,13 @@ export function Card({ product }) {
 
 
                 {productInWishlistStatus.length === 0 ?
-                    <button onClick={() => AddToWishlist(product, dispatch)}
+                    <button onClick={() => AddToWishlist(product, dispatch, loader, setLoader)}
                         class="basic-button">
                         <svg width="1em" height="1em" viewBox="0 0 16 16"><g><path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385c.92 1.815 2.834 3.989 6.286 6.357c3.452-2.368 5.365-4.542 6.286-6.357c.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path></g></svg>
                     </button>
                     :
 
-                    <button onClick={() => dispatch({ type: 'REMOVE_FROM_WISHLIST', payload: product })}
+                    <button onClick={() => RemoveFromWishlist(product, dispatch, loader, setLoader)}
                         class="basic-button">
                         <svg width="1em" height="1em" viewBox="0 0 16 16"><g> <path d="M8 1.314C12.438-3.248 23.534 4.735 8 15C-7.534 4.736 3.562-3.248 8 1.314z"></path></g></svg>
                     </button>
@@ -54,7 +54,7 @@ export function Card({ product }) {
                 <button
                     className={ifProductInCart.length > 0 ? clicked.currentClass : unclicked.currentClass}
                     disabled={ifProductInCart.length > 0 ? clicked.visibility : unclicked.visibility}
-                    onClick={() => AddToCart(product, dispatch)}>
+                    onClick={() => AddToCart(product, dispatch, loader, setLoader)}>
                     {ifProductInCart.length > 0 ? clicked.currentText : unclicked.currentText}
                 </button>
             </div>
