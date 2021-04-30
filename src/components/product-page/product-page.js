a33bbb2c5f8a27644a86d5c86756e621b4b3ae55
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-01 03:16:24 +0530

    style : ui fixes

--- src/components/product-page/product-page.js
+++ src/components/product-page/product-page.js
@@ -6,18 +6,19 @@ import { Sort } from '../Sort/Sort'
 import { Filters } from '../Filters/filters'
 import axios from 'axios'
 import { useDatabase } from '../DatabaseCalls/DatabaseCalls'
+import { Loader } from '../Loader/Loader'
 export function Products() {
-    const { state } = useEcom()
+    const { state, loader, setLoader } = useEcom()
     const { data } = state
     console.log("data ", data)
 
 
     return (
-        <div>
+        <div className="outer">
             <div className="inline-components">
                 <Filters />
             </div>
-
+            <Loader loader={loader} />
             <div className="products-main">
                 {data.map(product =>
                     <Card product={product} />
