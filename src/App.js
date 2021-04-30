1d10899a05e7a2aa46d5a182bcacb69ff9045509
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-01 03:14:23 +0530

    refactor : loader adjustment

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { Header } from './components/Header/header'
 import { Wishlist } from './components/Wishlist/wishlist'
 import { Cart } from './components/Cart/cart'
 import { Route, Routes } from 'react-router-dom'
-import axios from 'axios'
 import { useEcom } from './components/ecom-context/ecom-context';
 import { Loader } from './components/Loader/Loader';
+import { Homepage } from './components/Homepage/Homepage'
 
 function App() {
   const { loader } = useEcom()
   return (
     <div className="main-div">
-      <Loader loader={loader} />
+
       <div className="app-header"><Header /></div>
+      <Loader loader={loader} />
 
       <Routes>
-
+        <Route path="/" element={<div className="homepage"><Homepage /></div>} />
         <Route path="/wishlist" element={<div className="wishlist"><Wishlist /></div>} />
 
         <Route path="/products" element={<div className="products"><Products /></div>} />
