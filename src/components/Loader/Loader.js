41192c2c1931ad038406ae67ead740ac650491e9
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-01 03:15:42 +0530

    style : some changes to loader effects

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,22 +1,21 @@
 import './Loader.css'
 export function Loader({ loader }) {
     let visibilityStatus;
-    console.log("yahaaa tk to aarahaa hu")
     if (loader === true) {
         visibilityStatus = 'flex'
     }
     else {
         visibilityStatus = 'none'
-
     }
-    console.log("hey see i ran")
-
     return (
-        <div className="loader" style={{ display: visibilityStatus }}>
-            <span className="first-span"></span>
-            <span className="second-span"></span>
-            <span className="third-span"></span>
+        <div className="overlay" style={{ display: visibilityStatus }}>
+            <div className="loader" >
+                <span className="first-span"></span>
+                <span className="second-span"></span>
+                <span className="third-span"></span>
+            </div>
         </div>
+
     )
 
 }
