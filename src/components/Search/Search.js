f54646e7636a3fc79bb5962852538aa4547df4b9
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-02 00:08:16 +0530

    update : made a separate component for search functionality

---
+++ src/components/Search/Search.js
@@ -0,0 +1,29 @@
+import { useEcom } from "../ecom-context/ecom-context"
+import { Link } from 'react-router-dom'
+import { Filters } from '../Filters/filters'
+import { Card } from '../ProductCard/card'
+import { Loader } from '../Loader/Loader'
+export function Search() {
+    const { state, dispatch, loader, setLoader } = useEcom()
+    const { filteredProducts } = state
+    if (filteredProducts.length === 0) {
+        return (
+            <div className="search-container">
+                <h1>Oops!Your Search yeilded no result</h1>
+                <Link to='/products' className="button button-primary products-link" onClick={() => dispatch({ type: 'REMOVE_ALL_CONDITIONS' })}>Back to Products</Link>
+            </div>
+        )
+    }
+    return (
+        <div className="outer">
+            <div className="inline-components">
+            </div>
+            <Loader loader={loader} />
+            <div className="products-main">
+                {filteredProducts.map(product =>
+                    <Card product={product} />
+                )}
+            </div>
+        </div>
+    )
+}
