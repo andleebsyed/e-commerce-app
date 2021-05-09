be77939f147e51b1a8abc4b915fd6f3daa9cad55
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-09 18:21:14 +0530

    style : made account component responsive

--- src/components/PasswordReset/PasswordReset.js
+++ src/components/PasswordReset/PasswordReset.js
@@ -1,22 +1,28 @@
 import './PasswordReset.css'
 export function PasswordReset() {
     return (
-        <div className="account-info password-div">
-            <p className="label">Reset Password</p>
-            <div className="holder">
-                <label className="labels-acc" for="username">Current Password</label>
-                <input type="text" name="username" type="password" className="input-box acc-username" placeholder="" required />
-            </div>
-            <div className="holder">
-                <label className="labels-acc" for="username">New Password</label>
-                <input type="text" name="username" type="password" className="input-box acc-username" placeholder="" required />
-            </div>
+        <form>
+            <div className="account-info password-div">
+                <p className="label">Reset Password</p>
+                <div className="inline-inputs">
+                    <div className="holder internal-inline-div">
+                        <label className="labels-acc" for="username">Current Password</label>
+                        <input type="text" name="username" type="password" className="input-box acc-username" placeholder="" required />
+                    </div>
+                    <div className="holder internal-inline-div">
+                        <label className="labels-acc new-passsword" for="username">New Password</label>
+                        <input type="text" name="username" type="password" className="input-box acc-username" placeholder="" required />
+                    </div>
+                </div>
 
-            <div className="holder">
-                <label className="labels-acc" for="username">Confirm New Password</label>
-                <input type="text" name="username" type="password" className="input-box acc-username" placeholder="" required />
+
+                <div className="holder">
+                    <label className="labels-acc" for="username">Confirm New Password</label>
+                    <input type="text" name="username" type="password" className="input-box acc-username" placeholder="" required />
+                </div>
+                <button type="submit" className="button button-outline submit-button ">UPDATE PASSWORD</button>
             </div>
-            <button type="submit" className="button button-outline submit-button ">UPDATE PASSWORD</button>
-        </div>
+        </form>
+
     )
 }
--- src/components/ProfileDetails/ProfileDetails.js
+++ src/components/ProfileDetails/ProfileDetails.js
@@ -1,17 +1,20 @@
 import './ProfileDetails.css'
 export function ProfileDetails() {
     return (
-        <div className="account-info">
-            <p className="label">Account</p>
-            <div className="holder">
-                <label className="labels-acc" for="username">Username</label>
-                <input type="text" name="username" type="username" className="input-box acc-username" placeholder="username" required />
+        <form>
+            <div className="profile-details width-adjust">
+                <p className="label">Account</p>
+                <div className="holder">
+                    <label className="labels-acc" for="username">Username</label>
+                    <input type="text" name="username" type="username" className="input-box acc-username" placeholder="username" required />
+                </div>
+                <div className="holder">
+                    <label className="labels-acc" for="email">Email</label>
+                    <input type="text" name="email" type="email" className="input-box acc-password" placeholder="email" required />
+                </div>
+                <button type="submit" className="button button-outline submit-button ">UPDATE</button>
             </div>
-            <div className="holder">
-                <label className="labels-acc" for="email">Email</label>
-                <input type="text" name="email" type="email" className="input-box acc-password" placeholder="email" required />
-            </div>
-            <button type="submit" className="button button-outline submit-button ">UPDATE</button>
-        </div>
+        </form>
+
     )
 }
