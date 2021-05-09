cff815b079a9ec153a8a7afb3415cf17cbee10b7
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-10 02:15:00 +0530

    update : removed unused code

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -4,61 +4,10 @@ import { Link } from 'react-router-dom'
 import { useForm } from "react-hook-form";
 const LoginContext = createContext()
 export function Signup() {
-    // const { user, setUser } = useLogin()
-    let testuname = 'test'
-    let testpw = 'test123'
     const { register, handleSubmit } = useForm()
-    // const [username, setUsername] = useState('')
-    // const [password, setPassword] = useState('')
-    // const [innetText, setInnerText] = useState(false)
-    // const [failure, setFailure] = useState('none')
-
-    // useEffect(() => {
-    //     const loggedInUser = localStorage.getItem("user");
-    //     if (loggedInUser) {
-    //         const foundUser = JSON.parse(loggedInUser);
-    //         setUser(foundUser);
-    //     }
-    // }, [user]);
     const onSubmit = () => {
 
-        // let unBox = document.querySelector('.username');
-        // let pwBox = document.querySelector('.password');
-        // const data = { username, password }
-        // if (username === testuname && password === testpw) {
-        //     // set the state of the user
-        //     setUser(data)
-        //     // store the user in localStorage
-        //     localStorage.setItem('user', JSON.stringify(data))
-        // }
-        // else {
-        //     // unBox.innerText = ''
-        //     // pwBox.innetText = ''
-        //     setFailure('block')
-        // }}
     }
-
-
-
-    //     }
-    // }
-
-    // function handleLogout() {
-    //     setUser();
-    //     setUsername("");
-    //     setPassword("");
-    //     localStorage.clear();
-    // }
-    // if (user) {
-    //     return (
-    //         <div className="button-div">
-    //             <p className="welcome-text">Welcome {user.username}</p>
-    //             <button className="button button-primary button-custom" onClick={handleLogout}>Logout</button>
-    //         </div >
-
-    //     )
-    // }
-    // else {
     return (
         <form className="form" onSubmit={handleSubmit(onSubmit)}>
             <div className="login-main">
