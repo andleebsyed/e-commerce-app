8c3a740ab1a51053dcb98b4d1c5380c452ef483c
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-10 17:04:36 +0530

    update : made search box dynamic , will work only when input box has something in it

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -8,9 +8,17 @@ export function Header() {
     const { wishlist, cart } = state;
     const { user } = useLogin()
     const unLogged = 0;
+
+    let isAllowed = false
     console.log("user is ", user)
 
     const [comingProd, setComingProd] = useState('');
+    console.log("search should be allowed ", isAllowed)
+
+    // if input box has something in it do search otherwise don't
+    comingProd.length > 0 ? isAllowed = true : isAllowed = false
+    console.log("search should be allowed ", isAllowed)
+
     return (
         <div className="header-main">
             <nav className="navbar-main">
@@ -23,12 +31,26 @@ export function Header() {
 
                     </ul>
                 </div>
-                <div className="search-bar">
-                    <input type="text" id="search-box" className="search-box" placeholder="Search" onChange={(event) => setComingProd(event.target.value)} />
-                    <NavLink to='/search' className="search-box-button" activeClassName="selected" onClick={() => dispatch({ type: 'SEARCH_FOR_ITEM', payload: comingProd })} >
-                        <i class="material-icons">search</i>
-                    </NavLink>
-                </div>
+
+
+                {isAllowed ?
+                    <div className="search-bar">
+                        <input type="text" id="search-box" className="search-box" placeholder="Search" onChange={(event) => setComingProd(event.target.value)} />
+                        <NavLink disable={isAllowed} to='/search' className="search-box-button" activeClassName="selected" onClick={() => dispatch({ type: 'SEARCH_FOR_ITEM', payload: comingProd })} >
+                            <i class="material-icons">search</i>
+                        </NavLink>
+                    </div>
+
+                    :
+                    <div className="search-bar">
+                        <input type="text" id="search-box" className="search-box" placeholder="Search" onChange={(event) => setComingProd(event.target.value)} />
+                        <NavLink to='#' className="search-box-button" activeClassName="selected" >
+                            <i class="material-icons">search</i>
+                        </NavLink>
+                    </div>
+
+                }
+
                 <div className="navbar right-navbar-links">
                     <div className="individual-icons">
                         <div class="badge-container">
