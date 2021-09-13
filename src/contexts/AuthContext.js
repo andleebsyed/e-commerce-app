57575950ef3911c2a3ae55e6ff133d10b362f8f9
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-13 15:54:51 +0530

    update: added a case  whcih will turn true when axios interceptors are placed

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,6 +3,8 @@ import { createContext, useContext, useReducer } from "react";
 const AuthContext = createContext();
 function authReducer(state, { type, payload }) {
   switch (type) {
+    case "AUTH_SETUP":
+      return { ...state, authSetup: true };
     case "AUTHORIZE_USER":
       console.log({ payload });
       localStorage.setItem("token", payload.token);
@@ -11,6 +13,9 @@ function authReducer(state, { type, payload }) {
     case "LOGOUT_USER":
       localStorage.clear();
       return { ...state, authorized: false };
+    case "FETCH_ACCOUNT":
+      console.log({ payload }, " in reducer");
+      return { ...state, account: payload.account };
     default:
       return state;
   }
@@ -18,6 +23,8 @@ function authReducer(state, { type, payload }) {
 export function AuthProvider({ children }) {
   const [authState, dispatchAuth] = useReducer(authReducer, {
     authorized: localStorage.getItem("token") ? true : false,
+    account: null,
+    authSetup: false,
   });
   return (
     <AuthContext.Provider value={{ authState, dispatchAuth }}>
