fd3f0637f22a87ccb4e86763821c5ba4d635d261
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-13 15:55:40 +0530

    update: fetched account of logged in user for populating account component

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -4,21 +4,34 @@ import { ProfileDetails } from "../ProfileDetails/ProfileDetails";
 import { PasswordReset } from "../PasswordReset/PasswordReset";
 import { useAuth } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { BASE_URL } from "../../services/api";
+import { FetchAccount } from "../../services/users";
 // import { useEffect } from "react";
 export function Account() {
   //   const { user, setUser, token } = useLogin();
   const { authState, dispatchAuth } = useAuth();
-  const { authorized } = authState;
+  const { authorized, account, authSetup } = authState;
   const navigate = useNavigate();
+  console.log({ account });
   function handleLogout() {
     dispatchAuth({ type: "LOGOUT_USER" });
     navigate("/");
   }
+  useEffect(() => {
+    async function Run() {
+      const response = await FetchAccount();
+      dispatchAuth({ type: "FETCH_ACCOUNT", payload: response });
+    }
+    if (account === null && authSetup) {
+      Run();
+    }
+  }, [dispatchAuth, account, authSetup]);
   if (authorized) {
     return (
       <div className="outer-div">
         <div className="heading">
-          <h1 className="welcome-text">username</h1>
+          <h1 className="welcome-text">{account?.name}</h1>
           <button
             className="button button-primary button-custom"
             onClick={handleLogout}
