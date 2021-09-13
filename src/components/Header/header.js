577726396f782d605cc6b8cb3f4ffbb19f92c7b0
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-13 15:55:11 +0530

    refactor: removed unused code

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,76 +1,126 @@
-import './header.css'
-import { useEcom } from '../ecom-context/ecom-context'
-import { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom'
-import { useLogin } from '../Login/Login';
+import "./header.css";
+import { useEcom } from "../ecom-context/ecom-context";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { useLogin } from "../Login/Login";
 export function Header() {
-    const { state, dispatch } = useEcom();
-    const { wishlist, cart } = state;
-    const { user } = useLogin()
-    const unLogged = 0;
+  const { state, dispatch } = useEcom();
+  const { wishlist, cart } = state;
+  const { user } = useLogin();
+  const unLogged = 0;
 
-    let isAllowed = false
-    console.log("user is ", user)
+  let isAllowed = false;
 
-    const [comingProd, setComingProd] = useState('');
-    console.log("search should be allowed ", isAllowed)
+  const [comingProd, setComingProd] = useState("");
 
-    // if input box has something in it do search otherwise don't
-    comingProd.length > 0 ? isAllowed = true : isAllowed = false
-    console.log("search should be allowed ", isAllowed)
+  // if input box has something in it do search otherwise don't
+  comingProd.length > 0 ? (isAllowed = true) : (isAllowed = false);
 
-    return (
-        <div className="header-main">
-            <nav className="navbar-main">
-                <div>
-                    <ul className="navbar left-navbar-links">
+  return (
+    <div className="header-main">
+      <nav className="navbar-main">
+        <div>
+          <ul className="navbar left-navbar-links">
+            <NavLink
+              className="single-link"
+              end
+              activeClassName="selected"
+              to="/"
+            >
+              Home
+            </NavLink>
 
-                        <NavLink className="single-link" end activeClassName="selected" to='/'>Home</NavLink>
-
-                        <NavLink className="single-link" end activeClassName="selected" to='/products'>Products</NavLink>
-
-                    </ul>
-                </div>
-
-
-                {isAllowed ?
-                    <div className="search-bar">
-                        <input type="text" id="search-box" className="search-box" placeholder="Search" onChange={(event) => setComingProd(event.target.value)} />
-                        <NavLink disable={isAllowed} to='/search' className="search-box-button" activeClassName="selected" onClick={() => dispatch({ type: 'SEARCH_FOR_ITEM', payload: comingProd })} >
-                            <i class="material-icons">search</i>
-                        </NavLink>
-                    </div>
-
-                    :
-                    <div className="search-bar">
-                        <input type="text" id="search-box" className="search-box" placeholder="Search" onChange={(event) => setComingProd(event.target.value)} />
-                        <NavLink to='#' className="search-box-button" activeClassName="selected" >
-                            <i class="material-icons">search</i>
-                        </NavLink>
-                    </div>
-
-                }
-
-                <div className="navbar right-navbar-links">
-                    <div className="individual-icons">
-                        <div class="badge-container">
-                            <NavLink to='/wishlist' className="right-icons" activeClassName="selected"><i class="material-icons ">favorite_border</i></NavLink>
-                            <div class="badge-common badge-one">{user ? wishlist.length : 0}</div>
-                        </div>
-                    </div>
-                    <div class="individual-icons">
-                        <div class="badge-container ">
-                            <NavLink to='/account' className="right-icons" activeClassName="selected"><i class="material-icons "> account_circle </i></NavLink>
-                        </div>
-                    </div>
-                    <div class="individual-icons last-icon">
-                        <div class="badge-container">
-                            <NavLink to='/cart' className="right-icons" activeClassName="selected"><i class="material-icons"> add_shopping_cart </i></NavLink>
-                            <div class="badge-common badge-one">{user ? cart.length : 0}</div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+            <NavLink
+              className="single-link"
+              end
+              activeClassName="selected"
+              to="/products"
+            >
+              Products
+            </NavLink>
+          </ul>
         </div>
-    )
+
+        {isAllowed ? (
+          <div className="search-bar">
+            <input
+              type="text"
+              id="search-box"
+              className="search-box"
+              placeholder="Search"
+              onChange={(event) => setComingProd(event.target.value)}
+            />
+            <NavLink
+              disable={isAllowed}
+              to="/search"
+              className="search-box-button"
+              activeClassName="selected"
+              onClick={() =>
+                dispatch({ type: "SEARCH_FOR_ITEM", payload: comingProd })
+              }
+            >
+              <i class="material-icons">search</i>
+            </NavLink>
+          </div>
+        ) : (
+          <div className="search-bar">
+            <input
+              type="text"
+              id="search-box"
+              className="search-box"
+              placeholder="Search"
+              onChange={(event) => setComingProd(event.target.value)}
+            />
+            <NavLink
+              to="#"
+              className="search-box-button"
+              activeClassName="selected"
+            >
+              <i class="material-icons">search</i>
+            </NavLink>
+          </div>
+        )}
+
+        <div className="navbar right-navbar-links">
+          <div className="individual-icons">
+            <div class="badge-container">
+              <NavLink
+                to="/wishlist"
+                className="right-icons"
+                activeClassName="selected"
+              >
+                <i class="material-icons ">favorite_border</i>
+              </NavLink>
+              <div class="badge-common badge-one">
+                {user ? wishlist.length : 0}
+              </div>
+            </div>
+          </div>
+          <div class="individual-icons">
+            <div class="badge-container ">
+              <NavLink
+                to="/account"
+                className="right-icons"
+                activeClassName="selected"
+              >
+                <i class="material-icons "> account_circle </i>
+              </NavLink>
+            </div>
+          </div>
+          <div class="individual-icons last-icon">
+            <div class="badge-container">
+              <NavLink
+                to="/cart"
+                className="right-icons"
+                activeClassName="selected"
+              >
+                <i class="material-icons"> add_shopping_cart </i>
+              </NavLink>
+              <div class="badge-common badge-one">{user ? cart.length : 0}</div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
--- src/components/ecom-context/ecom-context.js
+++ src/components/ecom-context/ecom-context.js
@@ -1,106 +1,178 @@
-
-import { useContext, createContext, useReducer, useState, useEffect } from 'react'
-import axios from 'axios'
+import {
+  useContext,
+  createContext,
+  useReducer,
+  useState,
+  useEffect,
+} from "react";
+import axios from "axios";
 
 // creating context
-const EcomContext = createContext()
+const EcomContext = createContext();
 
 // reducer function
 function ecomReducer(state, { type, payload }) {
-    const { wishlist, cart, data, orgData, filteredProducts } = state
-    switch (type) {
-        case 'INITIAL_PRODUCTS':
-            return { ...state, data: [...payload], orgData: [...payload], filteredProducts: [] }
-        case 'INITIAL_CART':
-            return { ...state, cart: [...payload] }
-        case 'INITIAL_WISHLIST':
-            return { ...state, wishlist: [...payload] }
-        case 'ADD_TO_WISHLIST':
-            return { ...state, cart: cart.filter(item => item._id !== payload._id), wishlist: [...wishlist, payload] }
-        case 'ADD_TO_CART':
-            return { ...state, wishlist: wishlist.filter((item) => item._id !== payload._id), cart: [...cart, { ...payload, quantity: 1 }] }
-        case 'REMOVE_FROM_CART':
-            return { ...state, cart: cart.filter(product => product._id !== payload._id) }
-        case 'MOVE_TO_WISHLIST':
-            return { ...state, wishlist: [...wishlist, payload], cart: cart.filter(product => product._id !== payload._id) }
-        case 'REMOVE_FROM_WISHLIST':
-            return { ...state, wishlist: wishlist.filter(product => product._id !== payload._id) }
-        case 'INCREASE_QUANTITY':
-            return { ...state, cart: cart.map(product => product._id === payload._id ? { ...product, quantity: product.quantity + 1 } : { ...product }) }
-        case 'DECREASE_QUANTITY':
-            if (payload.quantity < 2) {
-                return { ...state, cart: cart.filter((product) => product._id !== payload._id) }
-            }
-            return { ...state, cart: cart.map(product => product._id === payload._id ? { ...product, quantity: product.quantity - 1 } : { ...product }) }
-        case 'SORT':
-            if (payload.target.value === 'low_to_high') { return { ...state, data: data.sort((a, b) => a.price - b.price) } } else { return { ...state, data: data.sort((a, b) => b.price - a.price) } }
-        case 'REMOVE_OUT_OF_STOCK':
-            if (payload === true) return { ...state, data: data.filter(product => product.inStock === true), searchStatus: false }; else return { ...state, data: orgData }
-        case "SHOW_FAST_DELIVERY_ONLY":
-            if (payload === true) return { ...state, data: data.filter(product => product.fastDelivery === true), searchStatus: false }; else return { ...state, data: orgData }
-        case 'SEARCH_FOR_ITEM':
-            return {
-                ...state, filteredProducts: orgData.filter(product =>
-                    product.name.toLowerCase().includes(payload.toLowerCase())
-                ), searchStatus: true
-            }
+  const { wishlist, cart, data, orgData, filteredProducts } = state;
+  switch (type) {
+    case "INITIAL_PRODUCTS":
+      return {
+        ...state,
+        data: [...payload],
+        orgData: [...payload],
+        filteredProducts: [],
+      };
+    case "INITIAL_CART":
+      return { ...state, cart: [...payload] };
+    case "INITIAL_WISHLIST":
+      return { ...state, wishlist: [...payload] };
+    case "ADD_TO_WISHLIST":
+      return {
+        ...state,
+        cart: cart.filter((item) => item._id !== payload._id),
+        wishlist: [...wishlist, payload],
+      };
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        wishlist: wishlist.filter((item) => item._id !== payload._id),
+        cart: [...cart, { ...payload, quantity: 1 }],
+      };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: cart.filter((product) => product._id !== payload._id),
+      };
+    case "MOVE_TO_WISHLIST":
+      return {
+        ...state,
+        wishlist: [...wishlist, payload],
+        cart: cart.filter((product) => product._id !== payload._id),
+      };
+    case "REMOVE_FROM_WISHLIST":
+      return {
+        ...state,
+        wishlist: wishlist.filter((product) => product._id !== payload._id),
+      };
+    case "INCREASE_QUANTITY":
+      return {
+        ...state,
+        cart: cart.map((product) =>
+          product._id === payload._id
+            ? { ...product, quantity: product.quantity + 1 }
+            : { ...product }
+        ),
+      };
+    case "DECREASE_QUANTITY":
+      if (payload.quantity < 2) {
+        return {
+          ...state,
+          cart: cart.filter((product) => product._id !== payload._id),
+        };
+      }
+      return {
+        ...state,
+        cart: cart.map((product) =>
+          product._id === payload._id
+            ? { ...product, quantity: product.quantity - 1 }
+            : { ...product }
+        ),
+      };
+    case "SORT":
+      if (payload.target.value === "low_to_high") {
+        return { ...state, data: data.sort((a, b) => a.price - b.price) };
+      } else {
+        return { ...state, data: data.sort((a, b) => b.price - a.price) };
+      }
+    case "REMOVE_OUT_OF_STOCK":
+      if (payload === true)
+        return {
+          ...state,
+          data: data.filter((product) => product.inStock === true),
+          searchStatus: false,
+        };
+      else return { ...state, data: orgData };
+    case "SHOW_FAST_DELIVERY_ONLY":
+      if (payload === true)
+        return {
+          ...state,
+          data: data.filter((product) => product.fastDelivery === true),
+          searchStatus: false,
+        };
+      else return { ...state, data: orgData };
+    case "SEARCH_FOR_ITEM":
+      return {
+        ...state,
+        filteredProducts: orgData.filter((product) =>
+          product.name.toLowerCase().includes(payload.toLowerCase())
+        ),
+        searchStatus: true,
+      };
 
-        case 'REMOVE_ALL_CONDITIONS':
-            return { ...state, data: orgData }
-        default:
-            return state
-
-    }
+    case "REMOVE_ALL_CONDITIONS":
+      return { ...state, data: orgData };
+    default:
+      return state;
+  }
 }
 
 export function EcomProvider({ children }) {
-    const [loader, setLoader] = useState(false)
-    const [state, dispatch] = useReducer(ecomReducer, { cart: [], wishlist: [], data: [], orgData: [], filteredProducts: [] })
-    console.log("state status ", state)
+  const [loader, setLoader] = useState(false);
+  const [state, dispatch] = useReducer(ecomReducer, {
+    cart: [],
+    wishlist: [],
+    data: [],
+    orgData: [],
+    filteredProducts: [],
+  });
 
+  // fetch products
+  useEffect(() => {
+    async function MyProducts() {
+      setLoader(true);
+      const response = await axios.get(
+        "https://rest-api.andydev7.repl.co/products"
+      );
+      dispatch({ type: "INITIAL_PRODUCTS", payload: response.data.myData });
+      setLoader(false);
+    }
+    MyProducts();
+  }, []);
 
-    // fetch products
-    useEffect(() => {
-        async function MyProducts() {
-            setLoader(true)
-            const response = await axios.get("https://rest-api.andydev7.repl.co/products")
-            dispatch({ type: 'INITIAL_PRODUCTS', payload: response.data.myData })
-            setLoader(false)
-        }
-        MyProducts()
-    }, []);
+  useEffect(() => {
+    async function MyCart() {
+      const response = await axios.get(
+        "https://rest-api.andydev7.repl.co/cart"
+      );
+      dispatch({ type: "INITIAL_CART", payload: response.data.myCart });
+    }
+    MyCart();
+  }, []);
 
-    useEffect(() => {
+  useEffect(() => {
+    async function MyWishlist() {
+      console.log("when is wishlist being fired");
+      const response = await axios.get(
+        "https://rest-api.andydev7.repl.co/wishlist"
+      );
+      console.log(
+        "wishlist coming from db ",
+        response.data.myWishlist,
+        " and its type is ",
+        typeof response.data.myWishlist
+      );
+      dispatch({ type: "INITIAL_WISHLIST", payload: response.data.myWishlist });
+    }
 
-        async function MyCart() {
-            const response = await axios.get('https://rest-api.andydev7.repl.co/cart')
-            dispatch({ type: 'INITIAL_CART', payload: response.data.myCart })
-        }
-        MyCart()
-    }, [])
+    MyWishlist();
+  }, []);
 
-    useEffect(() => {
-        async function MyWishlist() {
-            console.log("when is wishlist being fired")
-            const response = await axios.get('https://rest-api.andydev7.repl.co/wishlist')
-            console.log("wishlist coming from db ", response.data.myWishlist, " and its type is ", typeof (response.data.myWishlist))
-            dispatch({ type: 'INITIAL_WISHLIST', payload: response.data.myWishlist })
-        }
-
-        MyWishlist()
-
-    }, [])
-
-
-
-    return (
-        <EcomContext.Provider value={{ state, dispatch, loader, setLoader }}>
-            {children}
-        </EcomContext.Provider>
-    )
+  return (
+    <EcomContext.Provider value={{ state, dispatch, loader, setLoader }}>
+      {children}
+    </EcomContext.Provider>
+  );
 }
 
-
 export function useEcom() {
-    return useContext(EcomContext)
+  return useContext(EcomContext);
 }
