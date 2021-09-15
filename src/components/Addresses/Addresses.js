06a5b2b4fdcadd09fc81246f3faba57a7573a9cb
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-15 12:56:27 +0530

    style: styled the address modal

--- src/components/Addresses/Addresses.js
+++ src/components/Addresses/Addresses.js
@@ -1,16 +1,74 @@
 import "./addresses.css";
 import { useEcom } from "../ecom-context/ecom-context";
+import { useState } from "react";
 
 export function Addresses() {
   const { state } = useEcom();
   const { addresses } = state;
+  const [modalStatus, setModalStatus] = useState(false);
+  const [saveAddressButtonText, setSaveAddressButtonText] =
+    useState("Save Address");
+  const AddAddressModal = () => {
+    return (
+      <div className="address-modal-outer">
+        <div className="address-modal-main">
+          <button
+            style={{ marginLeft: "auto" }}
+            onClick={() => setModalStatus(false)}
+          >
+            X
+          </button>
+          <form className="address-form">
+            <input
+              className="address-column"
+              type="text"
+              name="name"
+              required
+              placeholder="Name"
+            />
+            <input
+              className="address-column"
+              type="text"
+              name="address"
+              required
+              placeholder="Address"
+            />
+            <input
+              className="address-column"
+              type="number"
+              name="pincode"
+              required
+              placeholder="Pincode"
+            />
+            <input
+              className="address-column"
+              type="text"
+              name="city"
+              required
+              placeholder="City"
+            />
+            <input
+              type="submit"
+              value={saveAddressButtonText}
+              className="button button-primary address-button"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  };
   return addresses === null ? (
     <div>loading...</div>
   ) : (
     <div className="address-main">
       <div className="address-heading">
         <p>Select Address</p>
-        <button className="button button-primary">Add new address</button>
+        <button
+          className="button button-primary"
+          onClick={() => setModalStatus(true)}
+        >
+          Add new address
+        </button>
       </div>
       {addresses.length === 0 ? (
         <div>
@@ -19,6 +77,7 @@ export function Addresses() {
       ) : (
         <div>These are my addressses</div>
       )}
+      {modalStatus && <AddAddressModal />}
     </div>
   );
 }
