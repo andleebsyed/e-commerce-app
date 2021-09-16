d670b1869d4db1115ba28f0b07aca380beb27f73
Syed Andleeb <andleebsyed75@gmail.com>
2021-09-16 14:57:18 +0530

    fix: had used class in place of className, fixed that

--- src/components/EmptyCart/EmptyCart.js
+++ src/components/EmptyCart/EmptyCart.js
@@ -1,11 +1,13 @@
-import './EmptyCart.css'
-import { Link } from 'react-router-dom'
+import "./EmptyCart.css";
+import { Link } from "react-router-dom";
 export function EmptyCart() {
-    return (
-        <div className="empty-wishlist-main">
-            <i class="material-icons large-cart">add_shopping_cart</i>
-            <strong>Looks like your Cart is empty</strong>
-            <Link to='/products' className="button button-primary link-style">Explore the wide range of Products</Link>
-        </div>
-    )
+  return (
+    <div className="empty-wishlist-main">
+      <i className="material-icons large-cart">add_shopping_cart</i>
+      <strong>Looks like your Cart is empty</strong>
+      <Link to="/products" className="button button-primary link-style">
+        Explore the wide range of Products
+      </Link>
+    </div>
+  );
 }
